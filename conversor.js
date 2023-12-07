0c5be3958418c0ff4437c428a165062f26ac8dfd
Pedro <pedrodutra2003@hotmail.com>
2023-12-07 16:25:29 -0300

    new file:   style.css

--- conversor.js
+++ conversor.js
@@ -1,17 +1,21 @@
 
 function bin2dec(){
+    //requisição
     var binario=document.getElementById("bin").value;
     var decimal=0;
     var verificado=0;
     var posicao=0;
+    //iteração para garantir que o numero é binario
     for(let i=0;i<binario.length;i++){
         let parte = parseInt(binario[i]);
         if(parte != 1 && parte != 0){
             alert("Digito diferente de 1 e 0 digitado: "+ parte);
+            return;
         }else{
             verificado+=1;
         }
     }
+    //conversão de bin para dec
     if (verificado == binario.length){
         for(;verificado-1>=0;verificado--){
             if(binario[verificado-1]==1){
@@ -20,8 +24,15 @@ function bin2dec(){
             posicao++;
         }
     }
+    //atribuindo o valor do decimal no documento
     document.getElementById("dec").value=decimal
     
     
 }
 
+function dec2bin(){
+
+}
+function start(){
+    document.getElementById("frombin");
+}
