d49c8962658fec7d48053d475c3621946a10687d
Pedro <pedrodutra2003@hotmail.com>
2023-12-07 14:25:37 -0300

    modified:   conversor.html 	modified:   conversor.js

--- conversor.js
+++ conversor.js
@@ -1,12 +1,27 @@
 
-function main()     {
-    var binario=document.getElementById("bin").textContent;
-
-    for(let i=0;i<=binario.length;i++){
-        if(binario[i]!= 1 || binario[i]!= 0){
-            alert("Digito diferente de 1 e 0 digitado");
+function bin2dec(){
+    var binario=document.getElementById("bin").value;
+    var decimal=0;
+    var verificado=0;
+    var posicao=0;
+    for(let i=0;i<binario.length;i++){
+        let parte = parseInt(binario[i]);
+        if(parte != 1 && parte != 0){
+            alert("Digito diferente de 1 e 0 digitado: "+ parte);
+        }else{
+            verificado+=1;
         }
     }
+    if (verificado == binario.length){
+        for(;verificado-1>=0;verificado--){
+            if(binario[verificado-1]==1){
+                decimal+=(2**posicao);
+            }
+            posicao++;
+        }
+    }
+    document.getElementById("dec").value=decimal
     
-    convert(binario);
+    
 }
+
