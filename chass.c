0677f19fadc17f274485bb4f9943949620032b18
PedroHenriqueDutra <pedrodutra2003@hotmail.com>
2024-03-06 10:47:32 -0300

    new file:   chass.c

---
+++ chass.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct peca{
+    int vazio;
+    int b;
+    int p;
+}peca;
+typedef struct jogo{
+    peca tabuleiro[8][8];
+    int pecab;
+    int pecap;
+}jogo;
+jogo start(){
+    jogo init;
+    init.pecab=init.pecap=16
+    for(int i=0; i < 8; i++){
+        if(i == 0 || i == 1 || i == 6 || i == 7 ){
+            for( int j = 0 ; j < 8 ; j++ ){
+                init.
+            }
+        }
+        
+    }
+}
+void main(){
+   jogo init = start();
+}
